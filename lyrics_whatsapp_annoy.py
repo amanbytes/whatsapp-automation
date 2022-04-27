d920ccda39f7634cf91701c47542670a43428ad5
Aman <56598905+amanbytes@users.noreply.github.com>
2022-04-27 07:32:31 +0530

    Add files via upload

---
+++ lyrics_whatsapp_annoy.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("user-data-dir=C:/Users/Aman/AppData/Local/Google/Chrome/User Data")
+driver = webdriver.Chrome('D:/Python/Small_Py_Projects/chromedriver.exe',chrome_options=options)
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 100)
+
+target = '"Target"'
+lyrics=open('lyrics.txt','r')
+text = lyrics.read()
+lyrics.close()
+text=text.split()
+lines=[]
+line=''
+for word in text:
+    if len(line)+len(word)>20:
+        lines.append(line)
+        line=''
+    line = line+' '+word
+lines.append(line)
+
+
+x_arg = '//span[contains(@title,'+ target +')]'
+group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+group_title.click()
+inp_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
+input_box = wait.until(EC.presence_of_element_located((By.XPATH, inp_xpath)))
+for word in lines:
+    input_box.send_keys(word+ Keys.ENTER)
+    time.sleep(0.2)
---
+++ what_auto.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+
+options=webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:/Users/Aman/AppData/Local/Google/Chrome/User Data")
+driver=webdriver.Chrome('chromedriver.exe',chrome_options=options)
+driver.get("https://web.whatsapp.com/")
+wait=WebDriverWait(driver,100)
+
+target='"Your Target"'
+message="Your Message"
+number_of_times=10 #No. of times to send a message
+
+contact_path='//span[contains(@title,'+ target +')]'
+contact=wait.until(EC.presence_of_element_located((By.XPATH,contact_path)))
+contact.click()
+message_box_path='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
+message_box=wait.until(EC.presence_of_element_located((By.XPATH,message_box_path)))
+for x in range(number_of_times):
+    message_box.send_keys(message + Keys.ENTER)
+    time.sleep(0.2)
